2b765b53b1d57e355f1c8d7be2310580b87ebe94
Junaid-coding <junaid.sindgi@gmail.com>
2025-09-13 11:34:36 +0100

    Add files via upload

---
+++ birthdays.py
@@ -0,0 +1,12 @@
+mom=9
+dad=14
+junaid=3
+print("Birthday date of mom is:",mom)
+print("Birthday date of dad is:",dad)
+print("Birthday date of Junaid is:",junaid)
+mom="November"
+dad="October"
+junaid="August"
+print("Birthday month of mom is:",mom)
+print("Birthday month of dad is:",dad)
+print("Birthday month of Junaid is:",junaid)
