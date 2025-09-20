8fd7d609e6444ca63b1a5037544c6eece91b5cd0
Junaid-coding <junaid.sindgi@gmail.com>
2025-09-20 11:40:21 +0100

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,14 @@
+name="Junaid"
+age=15
+isstudent=True
+ht=170.1
+print("name is:",name)
+print("data type of name is",type(name))
+print("age is:",age)
+print("data type of age is",type(age))
+print("is student:",isstudent)
+print("data type of is student is",type(isstudent))
+print("height is:",ht)
+print("data type of height is",type(ht))
+age=str(age)
+print("data type of age is",type(age))
