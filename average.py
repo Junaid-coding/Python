c8e9f3903300b4aa330ee3eec5cfe8ab9c96dc9b
Junaid-coding <junaid.sindgi@gmail.com>
2025-09-27 11:17:02 +0100

    Add files via upload

---
+++ average.py
@@ -0,0 +1,9 @@
+tree1 = 30
+tree2 = 29
+tree3 = 38
+tree4 = 21
+tree5 = 40
+sum = tree1+tree2+tree3+tree4+tree5
+print("sum of all the trees is", sum)
+average = sum/5
+print("Average of the trees is", average)
