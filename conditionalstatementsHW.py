27758e8203eac0b2aa590a0500d3406c69f10a81
Junaid-coding <junaid.sindgi@gmail.com>
2025-10-04 12:34:36 +0100

    Add files via upload

---
+++ conditionalstatementsHW.py
@@ -0,0 +1,7 @@
+temp = float(input("What is the current temperature: "))
+if (temp > 15):
+    print("You should wear a t-shirt and shorts")
+elif (temp > 5):
+    print("You should wear a fleece jacket and trousers")
+else:
+    print("You should wear a thick and warm jacket and thick trousers")
