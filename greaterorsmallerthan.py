19bb2fdcb8d62bfe8d24f20d0ca5bb1ea3e407b7
Junaid-coding <junaid.sindgi@gmail.com>
2025-10-04 11:38:09 +0100

    Add files via upload

---
+++ greaterorsmallerthan.py
@@ -0,0 +1,7 @@
+i = int(input("Enter a number: "))
+if (i > 15):
+    print("I am greater than 15")
+    print("I am in the if block")
+else:
+    print("I am smaller than 15")
+    print("I am in the else block")
