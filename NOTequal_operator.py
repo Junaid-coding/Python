596196bf771a7f02ed346292dfd3f2a797a94e6a
Junaid-coding <junaid.sindgi@gmail.com>
2025-10-11 11:37:59 +0100

    Add files via upload

---
+++ NOTequal_operator.py
@@ -0,0 +1,15 @@
+a = 2
+b = 3
+c = 3
+print(a != b)
+print(b != c)
+a = "python"
+b = "coding"
+if a != b:
+    print(a, "and", b, "are not the same")
+else:
+    print(a, "and", b, "are the same")
+a = 4
+b = 5
+if (a == 1) != (b == 5):
+    print("Hello")
