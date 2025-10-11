bcf3df1d477a10ef26f739afc933f2c9e670df51
Junaid-coding <junaid.sindgi@gmail.com>
2025-10-11 11:24:17 +0100

    Add files via upload

---
+++ ANDoperator.py
@@ -0,0 +1,7 @@
+a = 3
+b = -3
+c = -13
+if a and b and c:
+    print("All the number have the same boolean value")
+else:
+    print("Atleast one of the number dont have the same boolean value")
---
+++ ORoperator.py
@@ -0,0 +1,11 @@
+a = 10
+b = -10
+c = 0
+if a > 0 or b > 0:
+    print("Either of the numbers are greater than 0")
+else:
+    print("Both numbers are smaller than 0")
+if b > 0 or c > 0:
+    print("Either of the numbers are greater than 0")
+else:
+    print("Both the numbers are smaller than 0")
