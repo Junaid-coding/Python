f5f936cdfec1b6065bfac5c08fabedef035a1442
Junaid-coding <junaid.sindgi@gmail.com>
2025-10-25 13:30:57 +0100

    Add files via upload

---
+++ operatorprecedence.py
@@ -0,0 +1,13 @@
+x = 2
+y = 4
+w = 4
+v = 5
+z = (w+v)*x/y
+print("The value of (w + v) * x / y is", z)
+
+name = "alex"
+age = 0
+if name == "alex" or name == "john" and age >= 2:
+    print("Hello, welcome")
+else:
+    print("Goodbye!")
