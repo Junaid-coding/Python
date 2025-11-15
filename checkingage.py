72322a7c74aedd66b1442023e918dfc8c15178aa
Junaid-coding <junaid.sindgi@gmail.com>
2025-11-15 18:15:16 +0000

    Add files via upload

---
+++ checkingage.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+if age < 20:
+    if age > 10:
+        print("you are between 10 to 20 years old")
+    else:
+        print("you are not between 10 to 20 years old")
+else:
+    print("you are not between 10 to 20 years old")
