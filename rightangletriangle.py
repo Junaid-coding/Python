a9cccd20c2c1ef3718a5d3140c7dd13ed1a2be63
Junaid-coding <junaid.sindgi@gmail.com>
2025-12-06 12:27:30 +0000

    Add files via upload

---
+++ rightangletriangle.py
@@ -0,0 +1,6 @@
+print("Half pyramid pattern of stars (*): ")
+n = int(input("Enter the number of rows you want in the triangle: "))
+for i in range(n):
+    for j in range(i+1):
+        print("* ", end=" ")
+    print()
