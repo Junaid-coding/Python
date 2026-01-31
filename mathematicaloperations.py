624fd8e656535011b3b91a3ab10bfbab7fe0fa11
Junaid-coding <junaid.sindgi@gmail.com>
2026-01-31 12:58:10 +0000

    Add files via upload

---
+++ mathematicaloperations.py
@@ -0,0 +1,6 @@
+import math
+print("The floor and ceiling value of 23.56 are: " + str(math.ceil(23.56)) + " " + str(math.floor(23.56)))
+
+x = 10
+y = -15
+print("The value of x after copying the sign from y is: " + str(math.copysign(x,y)))
