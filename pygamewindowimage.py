abcdbd7ebdc13dbaa471934076746386b734c226
Junaid-coding <junaid.sindgi@gmail.com>
2026-06-27 19:37:52 +0100

    Add files via upload

---
+++ pygamewindowimage.py
@@ -0,0 +1,10 @@
+import pygame
+pygame.init()
+SCREEN_WIDTH,SCREEN_HEIGHT = 500, 500
+display_surface = pygame.display.set_model((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Adding image and background image')
+background_image = pygame.transform.scale(pygame.image.load('backgroundpng.htm').convert(),(SCREEN_WIDTH, SCREEN_HEIGHT))
+penguin_image = pygame.transform.scale(pygame.image.load('penguinpng.jfif'),convert_alpha(), (200, 200))
+penguin_rect = penguin_image.get_rect(center=(SCREEN_WIDTH//2,SCREEN_HEIGHT // 2 - 30))
+text = pygame.font.Font(None, 36).render('Hellow world!', True, pygame.Color('black'))
+text_rect = text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 110))
